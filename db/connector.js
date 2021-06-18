066e36a585b3da6dac2bf26ab7569a509ecfec59
Ayush Singh <ayushazuri@gmail.com>
2021-06-18 18:55:49 +0530

    Testing

--- db/connector.js
+++ db/connector.js
@@ -2,12 +2,15 @@ const mongoose = require("mongoose");
 
 const connectDB = (url) => {
 	// console.log(url);
-	return mongoose.connect(url, {
-		useNewUrlParser: true,
-		useCreateIndex: true,
-		useFindAndModify: false,
-		useUnifiedTopology: true,
-	});
+	return mongoose.connect(
+		"mongodb+srv://admin:9892770647@taskmanager.uystu.mongodb.net/TaskManager?retryWrites=true&w=majority",
+		{
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true,
+		}
+	);
 };
 
 module.exports = connectDB;
