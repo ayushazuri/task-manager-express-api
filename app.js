74dae8cebdd00e0a7f0656bed47f5dfef9390252
Ayush Singh <ayushazuri@gmail.com>
2021-06-19 14:02:57 +0530

    Deployement

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const tasks = require("./routes/tasks.js");
 const notFound = require("./middleware/not-found.js");
 const path = require("path");
-
+require("dotenv").config();
+// {
+// 	path: path.resolve(__dirname, "./.env");
+// }
 //Connections
 const app = express();
 const connectDB = require("./db/connector.js");
-require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 
 const PORT = process.env.PORT || 5000;
 
--- db/connector.js
+++ db/connector.js
@@ -2,15 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = (url) => {
 	// console.log(url);
-	return mongoose.connect(
-		"mongodb+srv://admin:9892770647@taskmanager.uystu.mongodb.net/TaskManager?retryWrites=true&w=majority",
-		{
-			useNewUrlParser: true,
-			useCreateIndex: true,
-			useFindAndModify: false,
-			useUnifiedTopology: true,
-		}
-	);
+	url =
+		"mongodb+srv://admin:9892770647@taskmanager.uystu.mongodb.net/TaskManager?retryWrites=true&w=majority";
+	return mongoose.connect(url, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true,
+	});
 };
 
 module.exports = connectDB;
